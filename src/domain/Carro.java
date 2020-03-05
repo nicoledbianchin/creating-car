4f28cd794ad97cca61fa835a324c8d0b7b9f9ca1
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-05 15:13:30 -0300

    implementa carro

---
+++ src/domain/Carro.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Carro {
+    private String modelo;
+    private String cor;
+    private int ano;
+    private String marca;
+    private int chassi;
+    private Proprietario proprietario;
+    private double velocidadeMaxima;
+    private double velocidadeAtual;
+    private int numeroPortas;
+    private boolean tetoSolar;
+    private int numeroMarchas;
+    private boolean cambioAutomatico;
+    private double volumeCombustivel;
+}
